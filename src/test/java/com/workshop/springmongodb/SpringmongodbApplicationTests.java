2e4a7231860a1ceea1c861736e0fb68e72b4aa00
Andev <andremoraes930@gmail.com>
2022-05-13 15:54:11 -0300

    First commit - Created project

---
+++ src/test/java/com/workshop/springmongodb/SpringmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
